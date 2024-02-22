a13a9c923d978965ee054eb5677a7307005a4dfe
sh1nheewon <tlsgmldnjs00@naver.com>
2024-02-22 12:51:22 +0900

    TodoList

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 - React v18 을 활용한 Front-End 프로젝트
 
-## React 프로젝트 만들기
-
-- `npx create-react-app --template=empty 프로젝트명`
+- `npm run start` : 실행
 
 - `npx create-react-app 프로젝트명` : `create-react-app` 도구를 최신버전을 받아(npx)서 `프로젝트명` 프로젝트를 생성하라.
 - `React` : 순수한 JS 를 이용한 Front-End 개발 도구
 
-- `npm run start` : 실행
+
+## React 프로젝트 만들기
+
+### 기본 개발도구만 설치된 프로젝트 만들기
+
+- `npx create-react-app --template=empty 프로젝트명`
+
+### React 의 기본 Template(Sample)가 포함된 프로젝트 만들기
+
+- `npx create-react-app 프로젝트명`
---
+++ react-todo-001/src/comps/TodoList.jsx
@@ -0,0 +1,33 @@
+
+// seq 와 todo 구성요소를 갖는 JSON 객체 데이터 3개를 포함하는 배열 
+// JSON : JavaScript Object Notation
+
+import { useState } from "react";
+
+// { 변수이름 : 값 }, { 변수1 : 값, 변수2 : 값 } 형태
+const todoListSample = [
+    { seq: 1, todo: "React 시작" },
+    { seq: 2, todo: "UI 구현" },
+    { seq: 3, todo: "Spring 서버" },
+    { seq: 4, todo: "점심에 뭐 먹을까" },
+    { seq: 5, todo: "오늘도 비가 온다" },
+    { seq: 6, todo: "내일은 더 많이 비온다" },
+]
+
+const TodoList = () => {
+    // todoListSample 데이터가 채워진 todoList 상태 변수배열 싲가하기
+    const [todoList, setTodoList] = useState(todoListSample);
+
+    // *****    exec/array.js 참조  *****
+    // todoList.forEach((todo) => console.log(todo));
+    // todoList.filter();
+    const viewList = todoList.map((item) => <div className="todoItem">{item.todo}</div>);
+    // todo item 을 받아 return, 배열의 개수만큼 나열 (item <- 설정한 이름)
+
+    return (
+        <>
+            {viewList}
+        </>
+    );
+};
+export default TodoList;
